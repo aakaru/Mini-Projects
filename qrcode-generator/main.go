496905244b999e5392c18d0e5c68f1d2b87c147a
Aakriti Kaushik <149955040+aakaru@users.noreply.github.com>
2025-03-27 22:51:02 +0530

    handle errors

--- qrcode-generator/main.go
+++ qrcode-generator/main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"github.com/skip2/go-qrcode"
 	"os"
 )
 
 func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go <data>")
+		return 
+	}
+
 	data := os.Args[1]
 
-	qrcode, _ := qrcode.Encode(data, qrcode.Medium, 256)
-
-	file, _ := os.Create("qrcode.png")
-
+	qrcode, err := qrcode.Encode(data, qrcode.Medium, 256)
+	if err != nil {
+		fmt.Println("Error generating QR code:", err)
+		return
+	}
+	
+	file, err := os.Create("qrcode.png")
+	if err != nil {
+		fmt.Println("Error creating file:", err)
+		return
+	}
 	defer file.Close()
-	file.Write(qrcode)
+
+	_, err = file.Write(qrcode)
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+	fmt.Println("QR code saved to qrcode.png")
 }
